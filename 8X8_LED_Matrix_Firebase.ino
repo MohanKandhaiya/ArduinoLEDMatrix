dd0af8ad33d914e0beb307d8c9ba833d75c3c55e
Mohan Kandhaiya <32198916+MohanKandhaiya@users.noreply.github.com>
2019-04-06 07:56:15 +0530

    Add files via upload

---
+++ 8X8_LED_Matrix_Firebase.ino
@@ -0,0 +1,213 @@
+// Including libraries of Firebase and WiFi 
+#include <FirebaseArduino.h>                                                // Firebase library - To connect to the Firebase database from Arduino clients
+#include <ESP8266WiFi.h>                                                    // WiFi library - To connect ESP8266 module to Wi-Fi network to start sending and receiving data
+#include <MaxMatrix.h>                                                      // LED Matrix Library - To communicate with the MAX7219 modules
+#include <SoftwareSerial.h>                                                 // To allow serial communication on other digital pins of the Arduino
+#include <avr/pgmspace.h>                                                   // To access data stored in program space (flash memory) of the device.
+
+#define FIREBASE_HOST "x8ledmatrixapp.firebaseio.com"                       // Link of API
+#define FIREBASE_AUTH "WDhtXxStr9h2bkXGLt755HTb7veYh6xpc2qBdKYV"            // Link of Database Secrets
+
+#define WIFI_SSID "MSK"                                                     // WiFi's Username
+#define WIFI_PASSWORD "Krishna0406"                                         // WiFi's Password
+
+#define WIFI_LED D5                                                         // Connect a LED to any of the GPIO pins of the board and replace pin number with it eg. D4                      
+
+
+PROGMEM const unsigned char CH[] = {
+  3, 8, B00000000, B00000000, B00000000, B00000000, B00000000, // space
+  1, 8, B01011111, B00000000, B00000000, B00000000, B00000000, // !
+  3, 8, B00000011, B00000000, B00000011, B00000000, B00000000, // "
+  5, 8, B00010100, B00111110, B00010100, B00111110, B00010100, // #
+  4, 8, B00100100, B01101010, B00101011, B00010010, B00000000, // $
+  5, 8, B01100011, B00010011, B00001000, B01100100, B01100011, // %
+  5, 8, B00110110, B01001001, B01010110, B00100000, B01010000, // &
+  1, 8, B00000011, B00000000, B00000000, B00000000, B00000000, // '
+  3, 8, B00011100, B00100010, B01000001, B00000000, B00000000, // (
+  3, 8, B01000001, B00100010, B00011100, B00000000, B00000000, // )
+  5, 8, B00101000, B00011000, B00001110, B00011000, B00101000, // *
+  5, 8, B00001000, B00001000, B00111110, B00001000, B00001000, // +
+  2, 8, B10110000, B01110000, B00000000, B00000000, B00000000, // ,
+  4, 8, B00001000, B00001000, B00001000, B00001000, B00000000, // -
+  2, 8, B01100000, B01100000, B00000000, B00000000, B00000000, // .
+  4, 8, B01100000, B00011000, B00000110, B00000001, B00000000, // /
+  4, 8, B00111110, B01000001, B01000001, B00111110, B00000000, // 0
+  3, 8, B01000010, B01111111, B01000000, B00000000, B00000000, // 1
+  4, 8, B01100010, B01010001, B01001001, B01000110, B00000000, // 2
+  4, 8, B00100010, B01000001, B01001001, B00110110, B00000000, // 3
+  4, 8, B00011000, B00010100, B00010010, B01111111, B00000000, // 4
+  4, 8, B00100111, B01000101, B01000101, B00111001, B00000000, // 5
+  4, 8, B00111110, B01001001, B01001001, B00110000, B00000000, // 6
+  4, 8, B01100001, B00010001, B00001001, B00000111, B00000000, // 7
+  4, 8, B00110110, B01001001, B01001001, B00110110, B00000000, // 8
+  4, 8, B00000110, B01001001, B01001001, B00111110, B00000000, // 9
+  2, 8, B01010000, B00000000, B00000000, B00000000, B00000000, // :
+  2, 8, B10000000, B01010000, B00000000, B00000000, B00000000, // ;
+  3, 8, B00010000, B00101000, B01000100, B00000000, B00000000, // <
+  3, 8, B00010100, B00010100, B00010100, B00000000, B00000000, // =
+  3, 8, B01000100, B00101000, B00010000, B00000000, B00000000, // >
+  4, 8, B00000010, B01011001, B00001001, B00000110, B00000000, // ?
+  5, 8, B00111110, B01001001, B01010101, B01011101, B00001110, // @
+  4, 8, B01111110, B00010001, B00010001, B01111110, B00000000, // A
+  4, 8, B01111111, B01001001, B01001001, B00110110, B00000000, // B
+  4, 8, B00111110, B01000001, B01000001, B00100010, B00000000, // C
+  4, 8, B01111111, B01000001, B01000001, B00111110, B00000000, // D
+  4, 8, B01111111, B01001001, B01001001, B01000001, B00000000, // E
+  4, 8, B01111111, B00001001, B00001001, B00000001, B00000000, // F
+  4, 8, B00111110, B01000001, B01001001, B01111010, B00000000, // G
+  4, 8, B01111111, B00001000, B00001000, B01111111, B00000000, // H
+  3, 8, B01000001, B01111111, B01000001, B00000000, B00000000, // I
+  4, 8, B00110000, B01000000, B01000001, B00111111, B00000000, // J
+  4, 8, B01111111, B00001000, B00010100, B01100011, B00000000, // K
+  4, 8, B01111111, B01000000, B01000000, B01000000, B00000000, // L
+  5, 8, B01111111, B00000010, B00001100, B00000010, B01111111, // M
+  5, 8, B01111111, B00000100, B00001000, B00010000, B01111111, // N
+  4, 8, B00111110, B01000001, B01000001, B00111110, B00000000, // O
+  4, 8, B01111111, B00001001, B00001001, B00000110, B00000000, // P
+  4, 8, B00111110, B01000001, B01000001, B10111110, B00000000, // Q
+  4, 8, B01111111, B00001001, B00001001, B01110110, B00000000, // R
+  4, 8, B01000110, B01001001, B01001001, B00110010, B00000000, // S
+  5, 8, B00000001, B00000001, B01111111, B00000001, B00000001, // T
+  4, 8, B00111111, B01000000, B01000000, B00111111, B00000000, // U
+  5, 8, B00001111, B00110000, B01000000, B00110000, B00001111, // V
+  5, 8, B00111111, B01000000, B00111000, B01000000, B00111111, // W
+  5, 8, B01100011, B00010100, B00001000, B00010100, B01100011, // X
+  5, 8, B00000111, B00001000, B01110000, B00001000, B00000111, // Y
+  4, 8, B01100001, B01010001, B01001001, B01000111, B00000000, // Z
+  2, 8, B01111111, B01000001, B00000000, B00000000, B00000000, // [
+  4, 8, B00000001, B00000110, B00011000, B01100000, B00000000, // \ backslash
+  2, 8, B01000001, B01111111, B00000000, B00000000, B00000000, // ]
+  3, 8, B00000010, B00000001, B00000010, B00000000, B00000000, // hat
+  4, 8, B01000000, B01000000, B01000000, B01000000, B00000000, // _
+  2, 8, B00000001, B00000010, B00000000, B00000000, B00000000, // `
+  4, 8, B00100000, B01010100, B01010100, B01111000, B00000000, // a
+  4, 8, B01111111, B01000100, B01000100, B00111000, B00000000, // b
+  4, 8, B00111000, B01000100, B01000100, B00101000, B00000000, // c
+  4, 8, B00111000, B01000100, B01000100, B01111111, B00000000, // d
+  4, 8, B00111000, B01010100, B01010100, B00011000, B00000000, // e
+  3, 8, B00000100, B01111110, B00000101, B00000000, B00000000, // f
+  4, 8, B10011000, B10100100, B10100100, B01111000, B00000000, // g
+  4, 8, B01111111, B00000100, B00000100, B01111000, B00000000, // h
+  3, 8, B01000100, B01111101, B01000000, B00000000, B00000000, // i
+  4, 8, B01000000, B10000000, B10000100, B01111101, B00000000, // j
+  4, 8, B01111111, B00010000, B00101000, B01000100, B00000000, // k
+  3, 8, B01000001, B01111111, B01000000, B00000000, B00000000, // l
+  5, 8, B01111100, B00000100, B01111100, B00000100, B01111000, // m
+  4, 8, B01111100, B00000100, B00000100, B01111000, B00000000, // n
+  4, 8, B00111000, B01000100, B01000100, B00111000, B00000000, // o
+  4, 8, B11111100, B00100100, B00100100, B00011000, B00000000, // p
+  4, 8, B00011000, B00100100, B00100100, B11111100, B00000000, // q
+  4, 8, B01111100, B00001000, B00000100, B00000100, B00000000, // r
+  4, 8, B01001000, B01010100, B01010100, B00100100, B00000000, // s
+  3, 8, B00000100, B00111111, B01000100, B00000000, B00000000, // t
+  4, 8, B00111100, B01000000, B01000000, B01111100, B00000000, // u
+  5, 8, B00011100, B00100000, B01000000, B00100000, B00011100, // v
+  5, 8, B00111100, B01000000, B00111100, B01000000, B00111100, // w
+  5, 8, B01000100, B00101000, B00010000, B00101000, B01000100, // x
+  4, 8, B10011100, B10100000, B10100000, B01111100, B00000000, // y
+  3, 8, B01100100, B01010100, B01001100, B00000000, B00000000, // z
+  3, 8, B00001000, B00110110, B01000001, B00000000, B00000000, // {
+  1, 8, B01111111, B00000000, B00000000, B00000000, B00000000, // |
+  3, 8, B01000001, B00110110, B00001000, B00000000, B00000000, // }
+  4, 8, B00001000, B00000100, B00001000, B00000100, B00000000, // ~
+};
+
+int DIN = D6;         // DIN pin of MAX7219 module
+int CLK = D8;         // CLK pin of MAX7219 module
+int CS = D7;          // CS pin of MAX7219 module
+int maxInUse = 2;     // Number of MAX7219's connected
+
+MaxMatrix m(DIN, CS, CLK, maxInUse);
+byte buffer[10];
+char incomebyte;
+int len = 1000;
+int scrollSpeed = 100;
+int brightness = 10;
+int count = 0;
+char indicator;
+char text[250];
+
+void setup() { 
+  // Start Serial Communication
+  Serial.begin(9600);
+
+  // MAX7219 Initialization
+  m.init(); 
+
+  // Initial LED Matrix Intensity, 0-15
+  m.setIntensity(brightness); 
+
+  // Defining the Pin Mode
+  pinMode(WIFI_LED,OUTPUT);      
+    
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);  
+  
+  // Connect to WiFi
+  while (WiFi.status() != WL_CONNECTED) {            // Wait till connected to WiFi
+  delay(100);  
+  digitalWrite(WIFI_LED,LOW);                        // Blink the light till connected to WiFi
+  delay(100);
+  digitalWrite(WIFI_LED,HIGH);
+  Serial.print("."); 
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  //connect to Database
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);      
+  delay(1000);
+  
+}
+
+void loop() {
+  // Getting the values entered in the Android App as String 
+  String data = Firebase.getString("Scrolling_Text"); 
+  yield();
+  Serial.println(data); 
+  // Converting the String to a Character Array
+  data.toCharArray(text, len);
+  // Calling the Function for Scrolling the Value
+  printStringWithShift(text, scrollSpeed);
+  delay(1000);
+}
+
+// Display the Extracted Characters with Scrolling
+void printCharWithShift(char c, int shift_speed) {
+  if (c < 32) return;
+  c -= 32;
+  memcpy_P(buffer, CH + 7 * c, 7);
+  m.writeSprite(32, 0, buffer);
+  m.setColumn(32 + buffer[0], 0);
+  for (int i = 0; i < buffer[0] + 1; i++)
+  {
+    delay(shift_speed);
+    m.shiftLeft(false, false);
+  }
+}
+
+// Extract the Characters from the Text String
+void printStringWithShift(char* s, int shift_speed) {
+  while (*s != 0) {
+    printCharWithShift(*s, shift_speed);
+    s++;
+  }
+}
+
+// Displaying the Extracted Characters without Scrolling
+void printString(char* s)
+{
+  int col = 0;
+  while (*s != 0)
+  {
+    if (*s < 32) continue;
+    char c = *s - 32;
+    memcpy_P(buffer, CH + 7 * c, 7);
+    m.writeSprite(col, 0, buffer);
+    m.setColumn(col + buffer[0], 0);
+    col += buffer[0] + 1;
+    s++;
+  }
+}
